e7db028d873893a6b2ca801ffe142b834b73350d
LuisRoca11 <lroca1102@gmail.com>
2025-09-18 14:18:05 -0300

    se agrega operador %

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Desarrollar un programa que permita cargar n números enteros y luego nos informe cuántos valores fueron pares y cuántos impares.
+// Emplear el operador “%” en la condición de la estructura condicional:
+// if (valor%2==0)         //Si el if da verdadero luego es par.
+
+using System;
+
+namespace n_pares_impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, pares = 0, impares = 0, fin = 1;
+
+            while (fin == 1)
+            {
+                Console.Write("Ingrese un numero entero: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    pares = pares + 1;
+                }
+                else
+                {
+                    impares = impares + 1;
+                }
+
+                Console.Write("Seguir ingresando numeros (si=1 no=0)");
+                fin = int.Parse(Console.ReadLine());
+
+                
+            }
+
+                Console.WriteLine($"La cantidad de numeros pares es: {pares}");
+                Console.WriteLine($"La cantidad de numeros impares es: {impares}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
